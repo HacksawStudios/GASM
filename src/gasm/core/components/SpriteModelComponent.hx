0093503c32e06160e4f7978abc69ee9f6ad72eb8
lbergman <lbergman8@gmail.com>
2018-04-04 16:01:12 +0200

    fix: Add dirty flag to sprite model component to reduce cpu usage

--- src/gasm/core/components/SpriteModelComponent.hx
+++ src/gasm/core/components/SpriteModelComponent.hx
@@ -12,14 +12,14 @@ import gasm.core.enums.EventType;
  * @author Leo Bergman
  */
 class SpriteModelComponent extends Component {
-    public var x(default, default):Float = 0;
-    public var y(default, default):Float = 0;
-    public var width(default, default):Float = -1;
-    public var height(default, default):Float = -1;
+    @:isVar public var x(default, set):Float = 0;
+    @:isVar public var y(default, set):Float = 0;
+    public var width(get, set):Float;
+    public var height(get, set):Float;
+    public var xScale(get, set):Float;
+    public var yScale(get, set):Float;
     public var origWidth(default, default):Float = -1;
     public var origHeight(default, default):Float = -1;
-    public var xScale(default, default):Float = 1;
-    public var yScale(default, default):Float = 1;
     public var mouseX(default, default):Float = 0;
     public var mouseY(default, default):Float = 0;
     public var stageMouseX(default, default):Float = 0;
@@ -32,6 +32,12 @@ class SpriteModelComponent extends Component {
     public var interactive(default, default):Bool = false;
     public var visible(default, default):Bool = true;
     public var mask(default, default):Any;
+    public var dirty(default, default):Bool = true;
+
+    var _width:Float = -1;
+    var _height:Float = -1;
+    var _xScale:Float = 1;
+    var _yScale:Float = 1;
 
     var _pressHandlers(default, default):Array<InteractionEvent -> Void>;
     var _overHandlers(default, default):Array<InteractionEvent -> Void>;
@@ -58,6 +64,14 @@ class SpriteModelComponent extends Component {
         for (type in Type.allEnums(EventType)) {
             removeHandlers(type);
         }
+        _pressHandlers = null;
+        _overHandlers = null;
+        _outHandlers = null;
+        _moveHandlers = null;
+        _dragHandlers = null;
+        _downHandlers = null;
+        _upHandlers = null;
+        _resizeHandlers = null;
     }
 
     public function addHandler(type:EventType, cb:InteractionEvent -> Void) {
@@ -67,9 +81,11 @@ class SpriteModelComponent extends Component {
 
     public function removeHandler(type:EventType, cb:InteractionEvent -> Void) {
         var handlers = getHandlers(type);
-        for (handler in handlers) {
-            if (handler == cb) {
-                handlers.remove(handler);
+        if(handlers != null) {
+            for (handler in handlers) {
+                if (handler == cb) {
+                    handlers.remove(handler);
+                }
             }
         }
     }
@@ -98,5 +114,69 @@ class SpriteModelComponent extends Component {
             case UP: _upHandlers;
             case RESIZE: _resizeHandlers;
         }
+    }
+
+    public function set_x(val:Float) {
+        x = val;
+        dirty = true;
+        return val;
+    }
+
+    public function set_y(val:Float) {
+        y = val;
+        dirty = true;
+        return val;
+    }
+
+    public function get_width() {
+        return _width;
+    }
+
+    public function set_width(val:Float) {
+        _width = val;
+        if(_width > 0 && origHeight > 0) {
+            _xScale = val / origWidth;
+        }
+        dirty = true;
+        return val;
+    }
+
+    public function get_height() {
+        return _height;
+    }
+
+    public function set_height(val:Float) {
+        _height = val;
+        if(_height > 0 && origHeight > 0) {
+            _yScale = val / origHeight;
+        }
+        dirty = true;
+        return val;
+    }
+
+    public function get_xScale() {
+        return _xScale;
+    }
+
+    public function set_xScale(val:Float) {
+        _xScale = val;
+        if(origWidth > 0) {
+            _width = _xScale * origWidth;
+        }
+        dirty = true;
+        return val;
+    }
+
+    public function get_yScale() {
+        return _yScale;
+    }
+
+    public function set_yScale(val:Float) {
+        _yScale = val;
+        if(origHeight > 0) {
+            _height = _yScale * origHeight;
+        }
+        dirty = true;
+        return val;
     }
 }
