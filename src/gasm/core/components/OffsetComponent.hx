b25381372444fc4cda51c717ce9786935570ab33
Sebastian Löfgren <sebastian@pandacreative.se>
2018-08-20 17:25:27 +0200

    Add scale argument for offset component

--- src/gasm/core/components/OffsetComponent.hx
+++ src/gasm/core/components/OffsetComponent.hx
@@ -7,15 +7,17 @@ import gasm.core.enums.ComponentType;
 
 class OffsetComponent extends Component {
     public var offset(default, null):Point;
+    private var _scale:Float;
 
-    public function new(?x:Float = 0, ?y:Float = 0) {
+    public function new(?x:Float = 0, ?y:Float = 0, scale = 1.0) {
         offset = {x:x, y:y};
         componentType = ComponentType.Actor;
+        _scale = scale;
     }
 
     override public function update(dt:Float) {
         var model:SpriteModelComponent = owner.get(SpriteModelComponent);
-        model.offsetX = offset.x;
-        model.offsetY = offset.y;
+        model.offsetX = offset.x * _scale;
+        model.offsetY = offset.y * _scale;
     }
 }
