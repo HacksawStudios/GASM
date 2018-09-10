936b6bb9190024a0093b686139af3df95e647ed5
lbergman <lbergman8@gmail.com>
2018-09-10 10:58:29 +0200

    feat: Apply scale to offset and layout

--- src/gasm/core/components/OffsetComponent.hx
+++ src/gasm/core/components/OffsetComponent.hx
@@ -2,22 +2,30 @@ package gasm.core.components;
 
 import gasm.core.math.geom.Point;
 import gasm.core.Component;
+import gasm.core.components.AppModelComponent;
 import gasm.core.components.SpriteModelComponent;
 import gasm.core.enums.ComponentType;
 
 class OffsetComponent extends Component {
-    public var offset(default, null):Point;
-    private var _scale:Float;
+	public var offset(default, null):Point;
 
-    public function new(?x:Float = 0, ?y:Float = 0, scale = 1.0) {
-        offset = {x:x, y:y};
-        componentType = ComponentType.Actor;
-        _scale = scale;
-    }
+	var _appModel:AppModelComponent;
+	var _scale:Float;
 
-    override public function update(dt:Float) {
-        var model:SpriteModelComponent = owner.get(SpriteModelComponent);
-        model.offsetX = offset.x * _scale;
-        model.offsetY = offset.y * _scale;
-    }
+	public function new(?x:Float = 0, ?y:Float = 0, scale = 1.0) {
+		offset = {x: x, y: y};
+		componentType = ComponentType.Actor;
+		_scale = scale;
+	}
+
+	override public function init() {
+		super.init();
+		_appModel = owner.getFromParents(AppModelComponent);
+	}
+
+	override public function update(dt:Float) {
+		var model:SpriteModelComponent = owner.get(SpriteModelComponent);
+		model.offsetX = offset.x * _scale * _appModel.scale;
+		model.offsetY = offset.y * _scale * _appModel.scale;
+	}
 }
