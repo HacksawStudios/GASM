112ba316741a787df53c1a50a078484e916b5e44
lbergman <lbergman8@gmail.com>
2019-03-08 16:43:42 +0100

    feat: Add scene system

--- src/gasm/core/enums/ComponentType.hx
+++ src/gasm/core/enums/ComponentType.hx
@@ -1,19 +1,21 @@
 package gasm.core.enums;
+
 /**
  * Flags for different component types.
- * 
+ *
  * @author Leo Bergman
  */
 enum ComponentType {
-    Model;
-    ActiveModel;
-    GraphicsModel;
-    Graphics3DModel;
-    TextModel;
-    SoundModel;
-    Actor;
-    Graphics;
-    Graphics3D;
-    Text;
-    Sound;
+	Model;
+	ActiveModel;
+	SceneModel;
+	GraphicsModel;
+	Graphics3DModel;
+	TextModel;
+	SoundModel;
+	Actor;
+	Graphics;
+	Graphics3D;
+	Text;
+	Sound;
 }
