36a3787ebffe5736af139ccf29df5950c511f967
Leo.Bergman <leo@hacksawstudios.com>
2020-03-30 15:22:34 +0200

    feat: Add per dveice shader enable/disable help

--- src/gasm/core/components/AppModelComponent.hx
+++ src/gasm/core/components/AppModelComponent.hx
@@ -1,16 +1,21 @@
 package gasm.core.components;
 
 import gasm.core.api.singnals.TResize;
-import gasm.core.enums.Orientation;
-import gasm.core.utils.Signal1;
-import gasm.core.math.geom.Point;
 import gasm.core.enums.ComponentType;
+import gasm.core.enums.Orientation;
+import gasm.core.math.geom.Point;
+import gasm.core.utils.Signal1;
+import haxe.ds.StringMap;
+
+using StringTools;
+using thx.Arrays;
 
 class AppModelComponent extends Component {
 	/**
 	 * Device orientation.
 	**/
 	public var orientation:Orientation;
+
 	public var stageSize:Point = {x: 0, y: 0};
 	public var scale:Float = 1.0;
 	public var resizeSignal:Signal1<TResize>;
@@ -20,10 +25,31 @@ class AppModelComponent extends Component {
 	public var freezeSignal:Signal1<Bool>;
 	public var customRenderCallback:Null<(engine:Any) -> Void> = null;
 	public var pixelRatio = 1.0;
+	public var assetsPath:String;
+	public var disableShaderDevices = new StringMap<Array<String>>();
+	public var enableShaderDevices = new StringMap<Array<String>>();
 
 	public function new() {
 		componentType = ComponentType.Model;
 		resizeSignal = new Signal1<TResize>();
 		freezeSignal = new Signal1<Bool>();
+	}
+
+	public function isShaderEnabled(name:String) {
+		#if js
+		final ua:String = js.Syntax.code('window.navigator.userAgent');
+		if (enableShaderDevices != null && enableShaderDevices.exists(name)) {
+			final enabled = enableShaderDevices.get(name).any(dev -> ua.contains(dev));
+			if (enabled) {
+				return true;
+			}
+		}
+		trace('disableShaderDevices', disableShaderDevices);
+		if (disableShaderDevices.exists(name)) {
+			final disabled = disableShaderDevices.get(name).any(dev -> ua.contains(dev));
+			return !disabled;
+		}
+		#end
+		return true;
 	}
 }
