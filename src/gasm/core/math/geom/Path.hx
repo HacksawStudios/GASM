04a8b5c646d6c5554b5e5c7a047ea44f0918a2db
ViktorHS <viktor@hacksawstudios.com>
2021-03-18 14:57:11 +0100

    chore: spelling

--- src/gasm/core/math/geom/Path.hx
+++ src/gasm/core/math/geom/Path.hx
@@ -7,7 +7,7 @@ import h3d.Vector;
 using Safety;
 
 /**
-	Abstract over Arrays of vectors providing useful functions when working with connected lines forming a path.
+	Abstract over Arrays of vectors providing useful functions when working with connecting lines forming a path.
 **/
 @:forward
 abstract Path(Array<Vector>) from Array<Vector> to Array<Vector> {
